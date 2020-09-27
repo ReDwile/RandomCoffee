7235f51703c74a5942dcc045cff2643286aab989
Ilistratov <i.listratov@yandex.ru>
2020-09-27 22:58:45 +0300

    Added Daclarations.py

---
+++ Declarations.py
@@ -0,0 +1,7 @@
+class DateBase:
+	pass
+
+
+class UserData:
+	pass
+
---
+++ test/tedt-sql.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conn = sqlite3.connect("test.db")
+# conn.execute("CREATE TABLE user_names(name TINYTEXT, handle TINYTEXT)")
+# conn.commit()
+# conn.execute('''INSERT INTO user_names VALUES('Ilia', 'Ilistratov')''')
+# conn.execute('''INSERT INTO user_names VALUES('Kirill', 'ReDwile')''')
+# conn.commit()
+c = conn.cursor()
+c.execute("SELECT name FROM user_names")
+print(c.fetchall()[0][0])
+conn.close()
